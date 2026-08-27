d2249625ac14ed291c56de8ebbc3bea13d9103e0
divykr2005 <maildivyansh2302@gmail.com>
2026-08-27 23:48:24 +0530

    fix: use mjs for next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Removed output: 'export' because of dynamic routes
-
 };
 
 export default nextConfig;
